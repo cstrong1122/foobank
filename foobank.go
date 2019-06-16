7f30029d43748c46b20e18cde3ce28e2f6ce0a3d
Christopher Strong <cstrong1122@gmail.com>
2019-06-16 15:52:38 -0400

    Initial commit

---
+++ foobank.go
@@ -0,0 +1,27 @@
+package foobank
+
+import (
+	"github.com/cstrong1122/moonlight"
+)
+
+type FooBankRateProvider struct {
+	parameters *moonlight.RateOptionParameters
+}
+
+func (b FooBankRateProvider) GetRateOptions(parameters *moonlight.RateOptionParameters) ([]moonlight.RateOption, error) {
+
+	options := []moonlight.RateOption{}
+	o1 := moonlight.RateOption{
+		InterestRate:      6.75,
+		DiscountPoints:    1.125,
+		OriginationPoints: .5}
+	options = append(options, o1)
+
+	o2 := moonlight.RateOption{
+		InterestRate:      6.5,
+		DiscountPoints:    1.25,
+		OriginationPoints: .75}
+	options = append(options, o2)
+
+	return options, nil
+}
